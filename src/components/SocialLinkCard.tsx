6b6b45ca7af3646458252e368e217617affc857c
Noel Dasco Jr. <79371033+Kiru100@users.noreply.github.com>
2024-05-22 23:23:48 +0800

    Update SocialLinkCard.tsx

--- src/components/SocialLinkCard.tsx
+++ src/components/SocialLinkCard.tsx
@@ -1,27 +1,28 @@
 import "./SocialLinkCard.scss";
+import jessica_image from "../assets/img/avatar-jessica.jpeg"
 
 const SocialLinkCard = () => {
     return (
         <main id="social_link_card">
-            <img src="https://i.pravatar.cc/150?img=39" alt="Profile Picture" className="profile_picture"/>
+            <img src={jessica_image} alt="Profile Picture" className="profile_picture"/>
             <h1 className="profile_name">Jessica Randal</h1>
             <p className="location">London, United Kingdom</p>
             <p className="bio">"Front-end developer and avid reader."</p>
             <ul className="links">
                 <li>
-                    <a href="" className="link_item">Github</a>
+                    <a href="/" className="link_item">Github</a>
                 </li>
                 <li>
-                    <a href="" className="link_item">Frontend Mentor</a>
+                    <a href="/" className="link_item">Frontend Mentor</a>
                 </li>
                 <li>
-                    <a href="" className="link_item">LinkedIn</a>
+                    <a href="/" className="link_item">LinkedIn</a>
                 </li>
                 <li>
-                    <a href="" className="link_item">Twitter</a>
+                    <a href="/" className="link_item">Twitter</a>
                 </li>
                 <li>
-                    <a href="" className="link_item">Instagram</a>
+                    <a href="/" className="link_item">Instagram</a>
                 </li>
             </ul>
         </main>
